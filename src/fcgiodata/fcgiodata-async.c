4267ee51b733c402905019b56c4f749c536a6d02
Michael Brown <Michael_E_Brown@Dell.com>
2015-03-27 05:40:57 +0000

    Start fleshing out threaded/async fcgi server for odata. still not hooked into odata, but it's now threaded and has backend process in place.
    Change-Id: I578baa16e286d0dab5a91453e127a3d92dda5b95

--- src/fcgiodata/fcgiodata-async.c
+++ src/fcgiodata/fcgiodata-async.c
@@ -1,46 +1,43 @@
 #include <pthread.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <syslog.h>
 
-#include "czmq.h"
-#include "fcgiapp.h"
+#include <czmq.h>
+#include <fcgiapp.h>
 
 #define THREAD_COUNT 20
 
-// zmq compat 1.4->3.0
-#if CZMQ_VERSION_MAJOR < 3
-#define zstr_send(dest, string) zstr_sendf(dest, "%s", string)
-#define zstr_sendm(dest, string) zstr_sendfm(dest, "%s", string)
-#endif
+#define DEBUG_OUT(fd, prio, args...) syslog(prio, args)
+#define DEBUG_PRINTF(args...) DEBUG_OUT(stderr, LOG_INFO, args)
 
+static int counts[THREAD_COUNT];
+static int running;
 
-#define DEBUG_PRINTF(X, ...)
-
-int running;
-
-static void *handle_request(void *a)
+static void *handle_cgi_request(void *a)
 {
-    long rc, thread_id = (long)a;
+    int rc, i; 
+    long thread_id = (long)a;
     pid_t pid = getpid();
     FCGX_Request request;
     char *server_name;
 
+    // create ZMQ context for IPC to our server task
     zctx_t *ctx = zctx_new ();
     void *cgiserver = zsocket_new (ctx, ZMQ_REQ);
 
     //  Set random identity to make tracing easier
-    char identity [10];
+    char identity [30]={0};
     sprintf (identity, "fcgi-listener-%ld", thread_id);
     zsocket_set_identity (cgiserver, identity);
     zsocket_connect (cgiserver, "tcp://localhost:5570");
 
     FCGX_InitRequest(&request, 0, 0);
 
-    DEBUG_PRINTF( "Thread %ld (%d) started\n", thread_id, pid);
-
     for (;;)
     {
         static pthread_mutex_t accept_mutex = PTHREAD_MUTEX_INITIALIZER;
+        static pthread_mutex_t counts_mutex = PTHREAD_MUTEX_INITIALIZER;
 
         /* Some platforms require accept() serialization, some don't.. */
         pthread_mutex_lock(&accept_mutex);
@@ -50,53 +47,98 @@ static void *handle_request(void *a)
         if (rc < 0)
             break;
 
-        int len;
-
-        //PrintEnv("REQUEST", environ);
-        char *contentLengthStr = FCGX_GetParam("CONTENT_LENGTH", request.envp);
-        if (contentLengthStr != NULL) {
-            len = strtol(contentLengthStr, NULL, 10);
-        }
-        else {
-            len = 0;
-        }
-
         server_name = FCGX_GetParam("SERVER_NAME", request.envp);
 
-        for(char **e = request.envp; e; e++)
-            zstr_sendfm(cgiserver, "%s", *e);
+        // Output some test data for now, until odataplugin is hooked in
+        FCGX_FPrintF(request.out,
+            "Content-type: text/html\r\n"
+            "\r\n"
+            "<title>FastCGI Hello! (multi-threaded C, fcgiapp library)</title>"
+            "<h1>FastCGI Hello! (multi-threaded C, fcgiapp library)</h1>"
+            "Thread %ld, Process %ld<p>"
+            "Request counts for %d threads running on host <i>%s</i><p><code>",
+            thread_id, pid, THREAD_COUNT, server_name ? server_name : "?");
 
-        zstr_sendfm(cgiserver, "");
+        int len = 0;
+        char *contentLengthStr = FCGX_GetParam("CONTENT_LENGTH", request.envp);
+        if (contentLengthStr != NULL)
+            len = strtol(contentLengthStr, NULL, 10);
 
+        // Send all HTTP HEADERS over to server process. One header per FRAME.
+        for(int i=0; request.envp[i]; i++)
+        {
+            char *p=request.envp[i];
+            zstr_sendfm(cgiserver, "%s", p);
+        }
+
+        // NULL Frame separates headers from content
+        zstr_sendf(cgiserver, "");
+
+        // Read Content from Apache over FCGI connection
+        // TODO: not sure if this is robust, check docs for best way... probably dont want to do char at a time
+        char *content = {0};
         if (len>0){
-            int i;
-            char *content = calloc(len+1, 1);
+            content = calloc(len+1, 1);
             char *c = content;
 
-            for (i = 0; i < len; i++) {
+            for (int i = 0; i < len; i++) {
                 if ((*c++ = getchar()) < 0) {
                     break;
 		        }
             }
-
-            zstr_sendf(cgiserver, "%s", content);
         }
 
-        FCGX_FPrintF(request.out,
-            "Content-type: text/html\r\n"
-            "\r\n"
-            "Hello world, from thread %ld (%d) on server %s",
-            thread_id, pid, server_name ? server_name : "?");
+        // Send HTTP CONTENT to server
+        zstr_sendf(cgiserver, "%s", content);
+        if(content)
+            free(content);
 
-        sleep(2);
+        pthread_mutex_lock(&counts_mutex);
+        ++counts[thread_id];
+        for (i = 0; i < THREAD_COUNT; i++)
+            FCGX_FPrintF(request.out, "%5d " , counts[i]);
+        pthread_mutex_unlock(&counts_mutex);
+
+        //**********************************
+        // RESPONSE
+        //
+        // print out the response we got back from the server
+
+        char *headers = NULL;
+        content = NULL;
+        zstr_recvx(cgiserver, &headers, &content);
+        DEBUG_PRINTF("GOT HEADERS: %s\n", headers);
+        DEBUG_PRINTF("GOT CONTENT: %s\n", content);
+        FCGX_FPrintF(request.out, "\n");
+        if(headers)
+            FCGX_FPrintF(request.out, "%s\r\n\r\n", headers);
+        if(content)
+            FCGX_FPrintF(request.out, "%s", content);
+        zstr_free(&headers);
+        zstr_free(&content);
+
         FCGX_Finish_r(&request);
     }
 
-    DEBUG_PRINTF( "Thread %ld (%d) exiting\n", thread_id, pid);
     zctx_destroy (&ctx);
     running=0;
 
     return NULL;
+}
+
+void process_msg(void *socket, zmsg_t *msg)
+{
+    zframe_t *identity = zmsg_first (msg);
+    zframe_t *frame = identity;
+    do {
+        char *foo = zframe_strdup(frame);
+        free(foo);
+    } while( (frame = zmsg_next(msg)) != 0 );
+
+    zframe_send(&identity, socket, ZFRAME_MORE | ZFRAME_REUSE );
+    zstr_sendfm(socket, "");
+    zstr_sendfm(socket, "headers");
+    zstr_sendf(socket, "content");
 }
 
 void *server_task (void *args)
@@ -106,17 +148,16 @@ void *server_task (void *args)
     void *cgiserver = zsocket_new (ctx, ZMQ_ROUTER);
     zsocket_bind (cgiserver, "tcp://*:5570");
     DEBUG_PRINTF("Starting up server task\n");
+    zsys_set_logsystem(1);
 
     zmq_pollitem_t items [] = { { cgiserver, 0, ZMQ_POLLIN, 0 } };
     while (running) {
         zmq_poll (items, 1, 100 * ZMQ_POLL_MSEC);
             if (items [0].revents & ZMQ_POLLIN) {
                 zmsg_t *msg = zmsg_recv (cgiserver);
-                zframe_t *identity = zmsg_pop (msg);
-                zframe_print (zmsg_last (msg), identity);
-                //zframe_t *content = zmsg_pop (msg);
+                process_msg(cgiserver, msg);
                 zmsg_destroy (&msg);
-            }
+           }
     }
 
     zctx_destroy (&ctx);
@@ -124,19 +165,20 @@ void *server_task (void *args)
     return NULL;
 }
 
+
 int main(void)
 {
-    DEBUG_PRINTF("Hello world.\n");
-    FCGX_Init();
-    DEBUG_PRINTF("Init complete.\n");
+    int i;
+    pthread_t id[THREAD_COUNT];
 
-    for (long i = 0; i < THREAD_COUNT; i++) {
-        DEBUG_PRINTF("Creating thread %ld\n", i);
-        zthread_new(handle_request, (void*)i);
-    }
-
-    DEBUG_PRINTF("Creating thread 0\n");
+    DEBUG_OUT(stderr, LOG_INFO, "hello world\n");
     running=1;
+
+    FCGX_Init();
+
+    for (i = 0; i < THREAD_COUNT; i++)
+        pthread_create(&id[i], NULL, handle_cgi_request, (void*)i);
+
     server_task(0);
 
     return 0;
